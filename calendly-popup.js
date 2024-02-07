9a491088372e66fe69db7b356a78dadf01c6b5ff
nbrasshands <86249681+nbrasshands@users.noreply.github.com>
2024-02-07 17:19:30 -0500

    Update calendly-popup.js
    Changed to trigger attribute vs ID

--- calendly-popup.js
+++ calendly-popup.js
@@ -1,23 +1,28 @@
-// This is V1.0 - Feb 7, 2024
+// This is V1.1 - Feb 7, 2024
+// Changed to trigger attribute instead of ID
 document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('calendlyButton');
-    button.addEventListener('click', function(e) {
-        e.preventDefault(); // Prevent default anchor behavior
+    // Select all elements with the attribute trainual-button="calendly"
+    var buttons = document.querySelectorAll('[trainual-button="calendly"]');
 
-        // Convert slashes to hyphens, and remove leading hyphen if present
-        var utmTerm = window.location.pathname.replace(/\//g, '-').replace(/^-/, ''); 
-        var encodedUTMTerm = encodeURIComponent(utmTerm);
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent default anchor behavior
 
-        // Retrieve and log UTM parameters from session storage with 'query.' prefix
-        var utmParams = getUTMParametersFromSession();
-        console.log("UTM Parameters from Session Storage:", utmParams); // Log retrieved UTM parameters
+            // Convert slashes in the pathname to hyphens, excluding the leading slash
+            var utmTerm = window.location.pathname.slice(1).replace(/\//g, '-');
+            var encodedUTMTerm = encodeURIComponent(utmTerm);
 
-        // Construct the Calendly URL with UTM parameters
-        var calendlyUrl = `https://calendly.com/d/2dz-wq6-6bk?utm_term=${encodedUTMTerm}${utmParams}`;
-        console.log("Final Calendly URL:", calendlyUrl); // Log the final URL
+            // Retrieve UTM parameters from session storage with 'query.' prefix
+            var utmParams = getUTMParametersFromSession();
+            console.log("UTM Parameters from Session Storage:", utmParams); // Log retrieved UTM parameters
 
-        Calendly.initPopupWidget({url: calendlyUrl});
-        return false;
+            // Construct the Calendly URL with UTM parameters
+            var calendlyUrl = `https://calendly.com/d/2dz-wq6-6bk?utm_term=${encodedUTMTerm}${utmParams}`;
+            console.log("Final Calendly URL:", calendlyUrl); // Log the final URL
+
+            Calendly.initPopupWidget({url: calendlyUrl});
+            return false;
+        });
     });
 });
 
@@ -25,7 +30,6 @@ function getUTMParametersFromSession() {
     const utmKeys = ['query.utm_source', 'query.utm_medium', 'query.utm_campaign', 'query.utm_content'];
     let utmParamsString = '';
 
-    // Iterate and log each check, accounting for 'query.' prefix
     utmKeys.forEach(key => {
         const value = sessionStorage.getItem(key);
         console.log(`Checking for ${key}:`, value); // Log each UTM parameter check
