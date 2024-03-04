4ad4e0b882d8d8d3dece6eee85f7d88570c33e37
nbrasshands <86249681+nbrasshands@users.noreply.github.com>
2024-03-04 09:31:17 -0500

    Update calendly-popup.js
    mobile update

--- calendly-popup.js
+++ calendly-popup.js
@@ -1,6 +1,7 @@
-// This is V1.2 - Feb 13, 2024
+// This is V1.3 - March 4, 2024
 // Changed to trigger attribute instead of ID
 // Added for the instance user is on homepage the UTM will update
+// Added for the instance user is on mobile the UTM will update
 document.addEventListener('DOMContentLoaded', function() {
     // Select all elements with the attribute trainual-button="calendly"
     var buttons = document.querySelectorAll('[trainual-button="calendly"]');
@@ -9,9 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function(e) {
             e.preventDefault(); // Prevent default anchor behavior
 
+            // Check if the user is on a mobile device
+            var isMobile = window.innerWidth <= 800; // or any other logic to determine mobile
+
             // Check if on the homepage or no URL path, default to "homepage", else convert slashes to hyphens
+            // If on mobile, set utmTerm to "mobile-responsive", otherwise use the page path
             var pathname = window.location.pathname;
-            var utmTerm = pathname === '/' || pathname === '' ? 'homepage' : pathname.slice(1).replace(/\//g, '-');
+            var utmTerm = isMobile ? 'mobile-responsive' : 
+                pathname === '/' || pathname === '' ? 'homepage' : pathname.slice(1).replace(/\//g, '-');
             var encodedUTMTerm = encodeURIComponent(utmTerm);
 
             // Retrieve UTM parameters from session storage with 'query.' prefix
@@ -27,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
 
 function getUTMParametersFromSession() {
     const utmKeys = ['query.utm_source', 'query.utm_medium', 'query.utm_campaign', 'query.utm_content'];
